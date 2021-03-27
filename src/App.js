0449c1f7e64b8624318d0db35ef387a148ed02f4
Subham <subham.sinha41@gmail.com>
2021-03-27 14:59:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import ProductListingPage from './pages/ProductListingPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
+      <div>
       <ProductListingPage />
+      <br />
+    </div>
+    <div>
+      <Footer />
+    </div>
     </div>
   );
 }
