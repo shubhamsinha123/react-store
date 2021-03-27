4ad1e48c61d00c812ea0733640873077ef9be130
Subham <subham.sinha41@gmail.com>
2021-03-27 15:55:57 +0530

    route commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
-import '../Footer.css'
+// import { Table } from 'react-bootstrap';
+// import '../Footer.css'
 
 const list1 = ["About Us", "Delivery Information","Privacy Policy","Terms & Conditions"];
 const list2 = ["Contact Us", "Returns", "Site Map"];
@@ -17,36 +17,42 @@ render() {
 <div class="col-sm-3">
 <h5>Information</h5>
 <ul class="list-unstyled">
-<li><a href="#">About Us</a></li>
-<li><a href="#">Delivery Information</a></li>
-<li><a href="#">Privacy Policy</a></li>
-<li><a href="#">Terms &amp; Conditions</a></li>
+<li><a href="#">{list1[0]}</a></li>
+<li><a href="#">{list1[1]}</a></li>
+<li><a href="#">{list1[2]}</a></li>
+<li><a href="#">{list1[3]}</a></li>
 </ul>
 </div>
 <div class="col-sm-3">
 <h5>Customer Service</h5>
 <ul class="list-unstyled">
-<li><a href="#">Contact Us</a></li>
-<li><a href="#">Returns</a></li>
-<li><a href="#">Site Map</a></li>
+<li><a href="#">{list2[0]}</a></li>
+<li><a href="#">{list2[1]}</a></li>
+<li><a href="#">{list2[2]}</a></li>
 </ul>
 </div>
 <div class="col-sm-3">
 <h5>Extras</h5>
 <ul class="list-unstyled">
-<li><a href="#">Brands</a></li>
-<li><a href="#">Gift Certificates</a></li>
-<li><a href="#">Affiliate</a></li>
-<li><a href="#">Specials</a></li>
+<li><a href="#">{list3[0]}</a></li>
+<li><a href="#">{list3[1]}</a></li>
+<li><a href="#">{list3[2]}</a></li>
+<li><a href="#">{list3[3]}</a></li>
 </ul>
 </div>
 <div class="col-sm-3">
 <h5>My Account</h5>
 <ul class="list-unstyled">
-<li><a href="#">My Account</a></li>
-<li><a href="#">Order History</a></li>
-<li><a href="#">Wish List</a></li>
-<li><a href="#">Newsletter</a></li>
+<li><a href="#">{list4[0]}</a></li>
+<li><a href="#">{list4[1]}</a></li>
+<li><a href="#">{list4[2]}</a></li>
+<li><a href="#">{list4[3]}</a></li>
+</ul>
+</div>
+<div class="col-sm-3">
+<ul class="list-unstyled">
+<li><a href="#">{list5[0]}</a></li>
+<li><a href="#">{list5[1]}</a></li>
 </ul>
 </div>
 </div>
